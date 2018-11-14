4973cb048155516298eac114f0cb5fb8dbfaed55
Matt Perrott <mperrott6@gmail.com>
2018-11-14 17:27:09 +1100

    Add functionality to get file info for project

---
+++ README.md
@@ -0,0 +1,16 @@
+## Quick notes
+
+- Node/express API to wrap the figma API
+- Implements logic to make calls from front end simple and only get the
+  info they need
+
+## Links
+
+- Deployed API:
+- Docs for figma API: https://documenter.getpostman.com/view/1836603/RzZAmJcw
+- Docs for figjam API: https://documenter.getpostman.com/view/1836603/RzZAmJcx
+
+## Setup
+
+1. Get token from figma
+2. `export TOKEN={figma-token}`
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+const request = require("request");
+
 // Set up the express app
 const app = express();
 
@@ -8,42 +10,59 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// var files = {};
+// Get the file information for particular project from figma API
+function getProjectFileDetails(idProject, callback) {
+  request(
+    {
+      url: "https://api.figma.com/v1/projects/" + idProject + "/files",
+      json: true,
+      headers: {
+        "X-Figma-Token": process.env.TOKEN
+      }
+    },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(body.files);
+      return callback(body.files);
+    }
+  );
+  // TODO: parse file info and save to files variable maybe?
+}
 
-// function updateFilenames() {
-
-// }
-
-// get all files
+// Get file information for file in project
+// Idea is to pick a file based on a "stage" param
+// e.g. ?stage=30 would get the file related to a 30% review
+// Note: This will probably be replaced by a more specific call e.g. frames
 app.get("/api/v1/:idProject/files", (req, res) => {
-  const idProject = parseInt(req.params.idProject, 1297611);
+  const idProject = parseInt(req.params.idProject, 10);
 
-  // get files in project
+  // 1. get files in project
+  // TEST: print the file details for project
+  getProjectFileDetails(idProject, function(response) {
+    var files = response;
+    console.log(files);
+  });
 
-  // get file information and check if current stage exists
+  // 2. check if file relating to stage exists
 
-  // check for any overwritten versions
+  // 3. check for any overwritten versions
 
-  // get correct version
+  // 4. get correct version (if there is an overwritten version)
 
-  // get frames for file
+  // 5. get frames for file
 
-  console.log(req.body.stage);
-  console.log(typeof req.body.version);
-  if (req.body.stage === "30") {
-    // get files for version 30
-    res.status(200).send({
-      success: "true",
-      message: "files retrieved successfully",
-      todos: ["file1", "file2"]
-    });
-  } else {
-    res.status(200).send({
-      success: "false",
-      message: "files found for that version"
-    });
-  }
+  // TEST: get files for version 30
+  res.status(200).send({
+    success: "true",
+    message: "files retrieved successfully",
+    // TODO need to replace with actual file information
+    files: ["file1", "file2"]
+  });
 });
+
+// Get port from process e.g. heroku or defaults to 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
